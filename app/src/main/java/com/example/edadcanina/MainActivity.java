6db2e1a5858326f73035e73bca820b0db784e871
PaulGonzalezHoshi <pafegoiv@gmail.com>
2024-09-18 20:13:09 -0600

    Se calcula la edad del perro

--- app/src/main/java/com/example/edadcanina/MainActivity.java
+++ app/src/main/java/com/example/edadcanina/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.edadcanina;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText edadedit = findViewById(R.id.age_id);
+        TextView resultText = findViewById(R.id.text_result);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String edad = edadedit.getText().toString();
+                int edadint = Integer.parseInt(edad);
+                int resultado = edadint * 7;
+                String resultadoString = "La edad de tu perro es " + resultado + " años";
+                resultText.setText(resultadoString);
+            }
+        });
     }
 }
